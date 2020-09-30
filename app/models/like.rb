0925ca8af76c0beb0e68b909896cb02a1cd29abd
Carlos Anriquez <canriquez@gmail.com>
2020-09-30 07:17:14 -0300

    add Like  model associations and model tests all green

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :application
+  belongs_to :admin, class_name: 'User'
+
+  validates :type, presence: true
+
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:admin)}
+  it { should belong_to(:application)}
+
+  it { should validate_presence_of(:type)}
+
 end
