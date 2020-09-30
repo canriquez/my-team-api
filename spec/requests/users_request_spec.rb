0f038903dcdf49f62a43b6752751dd1ac1ed0184
Carlos Anriquez <canriquez@gmail.com>
2020-09-30 18:29:58 -0300

    users_controller signup/#create method with specs green for admin and user roles

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,22 +3,28 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
 
     # initialize test data
-    let(:user) { build(:user_user) }  
+    let(:user) { build(:user_user) } 
+    let(:admin) { build(:admin_user) }
     # Build equates to #new metod. It submit properties and 
 
     # builds headers with no Authorization property on it to simulate the signup post
     let(:headers) { valid_headers.except('Authorization')} 
-    let(:valid_attributes) do
+    let(:valid_attributes_admin) do
+      attributes_for(:admin_user)
+    end
+
+    let(:valid_attributes_user) do
       attributes_for(:user_user)
     end
 
-    #signup test
+    #signup test for admin
     describe 'POST /signup' do
-      context 'when valid attributes' do
-        before { post '/signup', params: valid_attributes.to_json, headers: headers }
+      context 'when admin valid attributes' do
+        before { post '/signup', params: valid_attributes_admin.to_json, headers: headers }
 
         it 'creates a new user' do
-          p valid_attributes
+          p headers
+          p valid_attributes_admin
           expect(response).to have_http_status(201)
         end
 
@@ -30,6 +36,26 @@ RSpec.describe "Users", type: :request do
           expect(json['auth_token']).not_to be_nil
         end
       end
+
+      context 'when user valid attributes' do
+        before { post '/signup', params: valid_attributes_user.to_json, headers: headers }
+
+        it 'creates a new user' do
+          p headers
+          p valid_attributes_user
+          expect(response).to have_http_status(201)
+        end
+
+        it 'returns success message' do
+          expect(json['message']).to match(/Success! - Account has been created./)
+        end
+
+        it 'returns an authentication token' do
+          expect(json['auth_token']).not_to be_nil
+        end
+      end
+
+      
 
       context 'when invalid request' do
         before { post '/signup', params: {}, headers: headers }
